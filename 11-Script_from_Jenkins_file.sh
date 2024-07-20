c69f7b82265574d73bc626fa91b253d86aa76f4b
Mahesh-Gontla <115606905+Mahesh-Gontla@users.noreply.github.com>
2024-07-20 11:43:32 +0530

    Update 11-Script_from_Jenkins_file.sh

--- 11-Script_from_Jenkins_file.sh
+++ 11-Script_from_Jenkins_file.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 echo "Hello World"
+
+
+pipeline{
+    agent any
+    stages{
+        stage("git checkout stage"){
+            steps{
+                checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Mahesh-Gontla/Jenkins-Tasks-Udemy.git']])
+            }
+        }
+        stage("script usage"){
+            steps{
+                sh'''
+                chmod +x 11-Script_from_Jenkins_file.sh
+                sh 11-Script_from_Jenkins_file.sh
+                '''
+            }
+        }
+    }
+}
